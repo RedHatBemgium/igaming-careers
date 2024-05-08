deb1d7fd6a207eb7db328cfb46e249ea4965da68
RedHatBemgium <109520843+RedHatBemgium@users.noreply.github.com>
2024-05-08 21:58:03 +0000

    Database connected and job page made basic

--- database.py
+++ database.py
@@ -5,20 +5,28 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(
     db_connection_string,
     connect_args={
-        
+
     }
 )
 
-
-
-
-
-
-
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []    
-    for row in result.all():
-      jobs.append(row._asdict())
-    return jobs
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []    
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
+
+def load_job_from_db(id):
+    engine = create_engine(
+        db_connection_string,
+        connect_args={
+        }
+    )
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0]._asdict())
